e7b409d456dd7f3a37526e4505a37e08cd659beb
python-sd <dimitrov.s.dev@gmail.com>
2022-12-29 21:41:42 +0200

    More Exercises

--- Basic_Syntax_Conditional_Statements_Loops/EXERCISE/how_much_coffee_do_you_need.py
+++ Basic_Syntax_Conditional_Statements_Loops/EXERCISE/how_much_coffee_do_you_need.py
@@ -1,13 +1,12 @@
 command = input()
 coffee_count = 0
 while command.lower() != "end":
-	if "coding" in command or "dog" in command or "cat" in command \
-		or "movie" in command:
-		for char in command:
-			if char.isupper():
-				coffee_count += 2
-			else:
-				coffee_count += 1
+	if "coding" in command.lower() or "dog" in command.lower() or "cat" in command.lower() \
+		or "movie" in command.lower():
+		if command.isupper():
+			coffee_count += 2
+		else:
+			coffee_count += 1
 	if coffee_count > 5:
 		print("You need extra sleep")
 	command = input()
