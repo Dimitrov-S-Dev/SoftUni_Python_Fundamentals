c9c171d31dfb15bc00721c200e7e77a2ba44765f
python-sd <dimitrov.s.dev@gmail.com>
2022-12-29 11:42:08 +0200

    max_func - added

--- Basic_Syntax_Conditional_Statements_Loops/LAB/word_reverse.py
+++ Basic_Syntax_Conditional_Statements_Loops/LAB/word_reverse.py
@@ -1,5 +1,8 @@
 word = input()
-reversed_word = ""
-for index in range(len(word) - 1, -1, -1):
-	reversed_word += word[index]
+# reversed_word = ""
+# for index in range(len(word) - 1, -1, -1):
+# 	reversed_word += word[index]
+# print(reversed_word)
+reversed_word = word[::-1]
 print(reversed_word)
+
