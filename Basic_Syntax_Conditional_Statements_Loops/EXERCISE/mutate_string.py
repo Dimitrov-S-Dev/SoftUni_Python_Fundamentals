bfc134f6ee84dbcd1a320f948904ffc7fbdb7992
python-sd <dimitrov.s.dev@gmail.com>
2022-12-30 00:41:18 +0200

    More Exercises

--- Basic_Syntax_Conditional_Statements_Loops/EXERCISE/mutate_string.py
+++ Basic_Syntax_Conditional_Statements_Loops/EXERCISE/mutate_string.py
@@ -11,9 +11,11 @@ for index in range(len(first_word)):
     print(current_word)
     final_word = current_word
 
+# ------------>       alternative option      <------------------
 # for i in range(len(first_word)):
 # 	if first_word[i] != second_word[i]
 # 		replacement = second_word[i]
 # 		word = first_word[0:i] + replacement + second_word[i +1:]
 # 		first_word = word
 # 		print(word)
+#
