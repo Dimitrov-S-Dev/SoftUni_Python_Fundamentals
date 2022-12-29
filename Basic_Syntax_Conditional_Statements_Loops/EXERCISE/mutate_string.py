9778bdce69c5ece6e29c7e7374def6e43746d218
python-sd <dimitrov.s.dev@gmail.com>
2022-12-29 23:37:05 +0200

    More Exercises

--- Basic_Syntax_Conditional_Statements_Loops/EXERCISE/mutate_string.py
+++ Basic_Syntax_Conditional_Statements_Loops/EXERCISE/mutate_string.py
@@ -3,13 +3,13 @@ second_word = input()
 final_word = first_word
 
 for index in range(len(first_word)):
-	left_part = second_word[: index + 1]
-	right_part = first_word[index + 1:]
-	current_word = left_part + right_part
-	if current_word == final_word:
-		continue
-	print(current_word)
-	final_word = current_word
+    left_part = second_word[: index + 1]
+    right_part = first_word[index + 1:]
+    current_word = left_part + right_part
+    if current_word == final_word:
+        continue
+    print(current_word)
+    final_word = current_word
 
 # for i in range(len(first_word)):
 # 	if first_word[i] != second_word[i]
