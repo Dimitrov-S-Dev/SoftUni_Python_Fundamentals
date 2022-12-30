17518c7b8d1da9c87f17753bc0f3905df439391a
python-sd <dimitrov.s.dev@gmail.com>
2022-12-30 21:48:58 +0200

    Data_Types -> Exercise

--- Data_Types_and_Variables/Exercise/Elevator.py
+++ Data_Types_and_Variables/Exercise/Elevator.py
@@ -2,5 +2,8 @@ from math import ceil
 
 persons = int(input())
 capacity = int(input())
-courses = ceil(persons / capacity)
+courses = 0
+if courses != 0:
+    courses = ceil(persons / capacity)
+
 print(courses)
