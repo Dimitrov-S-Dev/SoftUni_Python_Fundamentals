8bdd9a9b56f23a2e8e91536e12e9059e19b4f8b4
Stan <dimitrov.s.dev@gmail.com>
2023-01-02 00:13:07 +0200

    Data_Types More Exercise

---
+++ Data_Types_and_Variables/More_Exercises/Exchange_integers.py
@@ -0,0 +1,9 @@
+first_num = int(input())
+second_num = int(input())
+print(f"Before:")
+print(f"a = {first_num}")
+print(f"b = {second_num}")
+first_num, second_num = second_num, first_num
+print(f"After:")
+print(f"a = {first_num}")
+print(f"b = {second_num}")
