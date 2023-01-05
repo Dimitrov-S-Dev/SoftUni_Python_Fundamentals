c3d4631fc248f7f6f6ba6d86088286ce772adf4f
Stan <dimitrov.s.dev@gmail.com>
2023-01-05 22:27:22 +0200

    Lists_Advanced_Lab

---
+++ Lists_Advanced/Lab/Palindrome_strongs.py
@@ -0,0 +1,7 @@
+words = input().split()
+palindrome = input()
+palindromes = [x for x in words if x == x[:: -1]]
+count = palindromes.count(palindrome)
+
+print(palindromes)
+print(f"Found palindrome {count} times")
