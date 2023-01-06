b9749debc2c1322b6d32ac4677c0a8f1546fd26d
Stan <dimitrov.s.dev@gmail.com>
2023-01-06 02:21:00 +0200

    Lists_Advanced

--- Lists_Advanced/Lab/The_office.py
+++ Lists_Advanced/Lab/The_office.py
@@ -13,4 +13,3 @@ if len(sad_employee) > len(happiness) / 2:
     print(f"Score: {len(happy_employee)}/{len(happiness)}.{not_happy_message}")
 else:
     print(f"Score: {len(happy_employee)}/{len(happiness)}.{happy_message}")
-
