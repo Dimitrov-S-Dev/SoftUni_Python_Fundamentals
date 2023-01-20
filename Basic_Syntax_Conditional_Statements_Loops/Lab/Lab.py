46df16a5d0599c3ebd69009882738a6d69fa5b93
Stan <dimitrov.s.dev@gmail.com>
2023-01-20 06:52:26 +0200

    Lab

---
+++ Basic_Syntax_Conditional_Statements_Loops/Lab/Lab.py
@@ -0,0 +1,93 @@
+# Task 1
+number = float(input())
+
+if number == 0:
+    print("zero")
+elif number > 0:
+    if number > 1_000_000:
+        print("large positive")
+    elif number < 1:
+        print("small positive")
+    else:
+        print("positive")
+else:
+    if abs(number) > 1_000_000:
+        print("large negative")
+    elif abs(number) < 1:
+        print("small negative")
+    else:
+        print("negative")
+
+# Task 2
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+# if first_num > second_num and first_num > third_num:
+# 	print(first_num)
+# elif second_num > first_num and second_num > third_num:
+# 	print(second_num)
+# else:
+# 	print(third_num)
+print(max(first_num, second_num, third_num))
+
+# Task 3
+
+word = input()
+# reversed_word = ""
+# for index in range(len(word) - 1, -1, -1):
+# 	reversed_word += word[index]
+# print(reversed_word)
+reversed_word = word[::-1]
+print(reversed_word)
+
+# Task 4
+
+count_iter = int(input())
+is_even = True
+
+for _ in range(count_iter):
+    number = int(input())
+    if number % 2 != 0:
+        is_even = False
+        print(f"{number} is odd!")
+        break
+    if is_even:
+        print("All numbers are even.")
+
+# Task 5
+
+while True:
+    number = float(input())
+    if 1 <= number <= 100:
+        print(f"The number {number} is between 1 and 100")
+        break
+
+# Task 6
+
+budget = int(input())
+is_all_bought = True
+command = input()
+
+while command != "End":
+    price = int(command)
+    if budget >= price:
+        budget -= price
+    else:
+        print("You went in overdraft!")
+        is_all_bought = False
+        break
+    command = input()
+
+if is_all_bought:
+    print("You bought everything needed.")
+
+# Task 7
+
+number = int(input())
+
+for row1 in range(1, number + 1):
+    print("*" * row1)
+
+for row2 in range(number - 1, 0, - 1):
+    print("*" * row2)
