6c355aa6852c511e4b05286f2c29c6a7d38a26e7
Stan <dimitrov.s.dev@gmail.com>
2023-01-20 07:28:39 +0200

    Lab

---
+++ Data_Types_and_Variables/Lab/Lab.py
@@ -0,0 +1,58 @@
+# Task 1
+
+first_name = input()
+second_name = input()
+delimiter = input()
+
+print(f"{first_name}{delimiter}{second_name}")
+
+# Task 2
+
+meters = int(input())
+kilometers = meters / 1000
+
+print(f"{kilometers:.2f}")
+
+# Task 3
+
+pounds = int(input())
+dollars = pounds * 1.31
+
+print(f"{dollars:.3f}")
+
+# Task 4
+
+centuries = int(input())
+years = centuries * 100
+days = int(years * 365.2422)
+hours = days * 24
+minutes = hours * 60
+
+print(f"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes")
+
+# Task 5
+
+count_iter = int(input())
+
+for number in range(1, count_iter + 1):
+    sum_of_digit = 0
+    sum_of_digit += number % 10
+    sum_of_digit += number // 10
+    if sum_of_digit == 5 or sum_of_digit == 7 or sum_of_digit == 14:
+        print(f"{number} -> True")
+    else:
+        print(f"{number} -> False")
+
+# Task 6
+
+year = int(input())
+
+while True:
+    year += 1
+    year_to_str = str(year)
+    set_year = set(year_to_str)
+    if len(year_to_str) != len(set_year):
+        continue
+    else:
+        print(year_to_str)
+        break
