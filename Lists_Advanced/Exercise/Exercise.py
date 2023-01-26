a2a2e601de4688af4fe77783609ee842af09fea0
Stan <dimitrov.s.dev@gmail.com>
2023-01-26 14:53:44 +0200

    Exercise

--- Lists_Advanced/Exercise/Exercise.py
+++ Lists_Advanced/Exercise/Exercise.py
@@ -31,6 +31,35 @@ result = [el for el in text if len(el) % 2 == 0]
 for word in result:
     print(word)
 
+# Task 4 Number Classification
 
+def get_positive(lst):
+    result = [str(el) for el in lst if el >= 0]
+    return ", ".join(result)
+
+
+def get_negative(lst):
+    result = [str(el) for el in lst if el < 0]
+    return ", ".join(result)
+
+
+def get_even(lst):
+    result = [str(el) for el in lst if el % 2 == 0]
+    return ", ".join(result)
+
+
+def get_odd(lst):
+    result = [str(el) for el in lst if el % 2 != 0]
+    return ", ".join(result)
+
+
+numbers = list(map(int, input().split(", ")))
+
+print(f"Positive: {get_positive(numbers)}")
+print(f"Negative: {get_negative(numbers)}")
+print(f"Even: {get_even(numbers)}")
+print(f"Odd: {get_odd(numbers)}")
+
+# Task 5 Office Chairs
 
 
