1b6ed70951df4acd6ba7bb1942555592b3ef0fc4
Stan <dimitrov.s.dev@gmail.com>
2023-01-26 15:04:22 +0200

    Exercise

--- Lists_Advanced/Exercise/Exercise.py
+++ Lists_Advanced/Exercise/Exercise.py
@@ -156,4 +156,38 @@ print(" ".join(decipher_message))
 # Task 9 Moving Target
 
 
+# Task 10 Heart Delivery
+
+house_number = list(map(int, input().split("@")))
+index = 0
+
+while True:
+    command = input()
+    if command == "Love!":
+        break
+    info = command.split()
+    jump = int(info[1])
+    current_index = index + jump
+
+    if current_index >= len(house_number):
+        current_index = 0
+    if house_number[current_index] == 0:
+        print(f"Place {current_index} already had Valentine's day.")
+    else:
+        house_number[current_index] -= 2
+        if house_number[current_index] == 0:
+            print(f"Place {current_index} has a Valentine's day")
+
+    index = current_index
+
+print(f"Cupid's last position was {index}")
+
+mission = [el for el in house_number if el != 0]
+if len(mission) > 0:
+    print(f"Cupid has failed {len(mission)} places.")
+else:
+    print(f"Mission was successful.")
+
+# Task 11 Inventory
+
 
