bd09851415afb80a7dd0d88b3ef9ac2c1011878e
Stan <dimitrov.s.dev@gmail.com>
2023-01-26 14:59:10 +0200

    Exercise

--- Lists_Advanced/Exercise/Exercise.py
+++ Lists_Advanced/Exercise/Exercise.py
@@ -117,5 +117,41 @@ print(f"Group of 30's: {gr_30}")
 print(f"Group of 40's: {gr_40}")
 print(f"Group of 50's: {gr_50}")
 
+# Task 8 Decipher This!
+
+# messages = input().split()
+# final_message = []
+#
+# for message in messages:
+#     number = ""
+#     current_message = ""
+#     for character in message:
+#         if character.isdigit():
+#             number += character
+#         else:
+#             break
+#     message = message.replace(number, "")
+#     number = int(number)
+#     current_message += chr(number)
+#     if len(message) > 1:
+#         message = message[-1] + message[1: - 1] + message[0]
+#     current_message += message
+#     final_message.append(current_message)
+#
+# print(" ".join(final_message))
+
+messages = input().split()
+decipher_message = []
+
+for message in messages:
+    char = [el for el in message if not el.isdigit()]
+    digit = [el for el in message if el.isdigit()]
+    char_digit = [chr(int("".join(digit)))]
+    char[0], char[-1] = char[-1], char[0]
+    new_message = char_digit + char
+    decipher_message.append("".join(new_message))
+
+print(" ".join(decipher_message))
+
 
 
