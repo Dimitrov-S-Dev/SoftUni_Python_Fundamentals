200f4f0fa1805341e7c77ebdbf988b94103afffd
Stan <dimitrov.s.dev@gmail.com>
2023-01-26 14:57:51 +0200

    Exercise

--- Lists_Advanced/Exercise/Exercise.py
+++ Lists_Advanced/Exercise/Exercise.py
@@ -102,4 +102,20 @@ def get_electron(num):
 electron = int(input())
 print(get_electron(electron))
 
+# Task 7 Group of 10's
+
+lst = list(map(int, input().split(", ")))
+gr_10 = [num for num in lst if 0 <= num <= 10]
+gr_20 = [num for num in lst if 11 <= num <= 20]
+gr_30 = [num for num in lst if 21 <= num <= 30]
+gr_40 = [num for num in lst if 31 <= num <= 40]
+gr_50 = [num for num in lst if 41 <= num <= 50]
+
+print(f"Group of 10's: {gr_10}")
+print(f"Group of 20's: {gr_20}")
+print(f"Group of 30's: {gr_30}")
+print(f"Group of 40's: {gr_40}")
+print(f"Group of 50's: {gr_50}")
+
+
 
