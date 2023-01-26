e72e079b99a6dd0298b7dd12e16bb6b816426f44
Stan <dimitrov.s.dev@gmail.com>
2023-01-26 14:51:05 +0200

    Exercise

--- Lists_Advanced/Exercise/Exercise.py
+++ Lists_Advanced/Exercise/Exercise.py
@@ -10,3 +10,17 @@ for w1 in first:
             result.append(w1)
 
 print(result)
+
+# Task 2 Next Version
+
+
+def get_version(lst):
+    version_int = int("".join(lst)) + 1
+    version_lst = [num for num in str(version_int)]
+    print(".".join(version_lst))
+
+
+version_str = input().split(".")
+get_version(version_str)
+
+
