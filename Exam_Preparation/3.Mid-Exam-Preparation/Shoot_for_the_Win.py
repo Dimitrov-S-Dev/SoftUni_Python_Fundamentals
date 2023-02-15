3ee91e0e52e7a350848a598df87fe9522cdd2100
Stan <dimitrov.s.dev@gmail.com>
2023-02-15 02:42:47 +0200

    Exams

--- Exam_Preparation/3.Mid-Exam-Preparation/Shoot_for_the_Win.py
+++ Exam_Preparation/3.Mid-Exam-Preparation/Shoot_for_the_Win.py
@@ -18,9 +18,11 @@ def get_main(lst):
         if command == "End":
             break
         index = int(command)
+
         if index in range(len(lst)) and lst[index] != -1:
             lst = get_shot(lst, index)
             shot_target += 1
+
     result = [str(num) for num in lst]
     return f"Shot targets: {shot_target} -> {' '.join(result)}"
 
