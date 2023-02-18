daed1e2850b5a6675ce417659ba9f94ea51d08d8
Stan <dimitrov.s.dev@gmail.com>
2023-02-18 03:14:50 +0200

    Exams

--- Exam_Preparation/6.Mid-Exam-Preparation/Bonus_Scoring_System.py
+++ Exam_Preparation/6.Mid-Exam-Preparation/Bonus_Scoring_System.py
@@ -1,3 +1,6 @@
 number_of_students = int(input())
 number_of_lectures = int(input())
 additional_bonus = int(input())
+
+for student in range(number_of_students):
+    current_attendance = int(input())
