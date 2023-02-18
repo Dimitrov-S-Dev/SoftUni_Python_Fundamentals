a7bc0adeb980c585b611471fff117f0c94fa2dc5
Stan <dimitrov.s.dev@gmail.com>
2023-02-18 03:22:32 +0200

    Exams

--- Exam_Preparation/6.Mid-Exam-Preparation/Bonus_Scoring_System.py
+++ Exam_Preparation/6.Mid-Exam-Preparation/Bonus_Scoring_System.py
@@ -1,6 +1,16 @@
 number_of_students = int(input())
 number_of_lectures = int(input())
 additional_bonus = int(input())
+max_bonus = 0
+max_attendance = 0
 
 for student in range(number_of_students):
+    total_bonus = 0
     current_attendance = int(input())
+    total_bonus += round((current_attendance) / (number_of_lectures) * (5 + additional_bonus))
+    if total_bonus > max_bonus:
+        max_bonus = total_bonus
+        max_attendance = current_attendance
+
+print(max_bonus)
+print(max_attendance)
