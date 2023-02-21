651c644e2ecc5509233f03f77812901062a07d2a
Stan <dimitrov.s.dev@gmail.com>
2023-02-21 07:13:32 +0200

    Classes_and_Objects_Lab

--- Objects_and_Classes/Lab/Lab.py
+++ Objects_and_Classes/Lab/Lab.py
@@ -62,5 +62,51 @@ send_email = [emails[int(x)].send() for x in input().split(", ")]
 for email in emails:
     print(email.get_info())
 
+Task 4 Zoo
 
+class Zoo:
+    __animals = 0
+
+    def __init__(self, name):
+        self.name = name
+        self.mammals = []
+        self.fishes = []
+        self.birds = []
+
+    def add_animal(self, species, name):
+        if species == "mammals":
+            self.mammals.append(name)
+        elif species == "fishes":
+            self.fishes.append(name)
+        elif species == "birds":
+            self.birds.append(name)
+        Zoo.__animals += 1
+
+    def get_info(self, species):
+        result = ""
+        if species == "mammal":
+            result += f"Mammal in {self.name}: {', '.join(self.mammals)}\n"
+        elif species == "fish":
+            result += f"Fishes in {self.name}: {', '.join(self.fishes)}\n"
+        elif species == "bird":
+            result += f"Birds in {self.name}: {', '.join(self.birds)}\n"
+
+        result += f"Total animals: {Zoo.__animals}"
+        return result
+
+zoo_name = input()
+zoo = Zoo(zoo_name)
+counter = int(input())
+
+for i in range(counter):
+    species, name = input().split()
+    if species == "mammals":
+        zoo.add_animal(species, name)
+    elif species == "fishes":
+        zoo.add_animal(species, name)
+    elif species == "birds":
+        zoo.add_animal(species, name)
+
+species_name = input()
+print(zoo.get_info(species_name))
 
