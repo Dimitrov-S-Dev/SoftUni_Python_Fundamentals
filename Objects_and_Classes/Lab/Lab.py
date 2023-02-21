4a78643ae2282c4b761b0ec31337efd414d940ee
Stan <dimitrov.s.dev@gmail.com>
2023-02-21 05:21:58 +0200

    Classes_and_Objects_Lab

--- Objects_and_Classes/Lab/Lab.py
+++ Objects_and_Classes/Lab/Lab.py
@@ -12,3 +12,23 @@ comment = Comment("user1", "I like this book")
 print(comment.username)
 print(comment.content)
 print(comment.likes)
+
+# Task 2 Party
+
+class Party:
+
+    def __init__(self):
+        self.people = []
+
+persons = Party()
+
+while True:
+    name = input()
+    if name == "End":
+        break
+    persons.people.append(name)
+
+print(f"Going: ', '.join{persons.people}")
+print(f"Total: {len(persons.people)}")
+
+
