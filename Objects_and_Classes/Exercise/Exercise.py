a4ecac5b67014cb53418b062871e3c67d091a847
Stan <dimitrov.s.dev@gmail.com>
2023-02-21 22:57:59 +0200

    Classes_and_Objects_Lab

--- Objects_and_Classes/Exercise/Exercise.py
+++ Objects_and_Classes/Exercise/Exercise.py
@@ -125,3 +125,30 @@ a_class.add_student("Peter", 4.80)
 a_class.add_student("George", 6.00)
 a_class.add_student("Amy", 3.50)
 print(a_class)
+
+# Task 6 Inventory
+
+class Inventory:
+    
+    def __init__(self, __capacity:int):
+        self.__capacity = __capacity
+        self.items = []
+        
+    def add_item(self, item:str):
+        if self.__capacity > len(self.items):
+            self.items.append(item)
+            self.__capacity -= 1
+        return f"not enough room in the inventory"
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def __repr__(self):
+        return f"Items: {', '.join(self.items)}.\nCapacity left: {self.get_capacity()}"
+
+inventory = Inventory(2)
+inventory.add_item("potion")
+inventory.add_item("sword")
+print(inventory.add_item("bottle"))
+print(inventory.get_capacity())
+print(inventory)
