67eb5ab7a80dbc8c396f62252d86be0ab8aa9a68
Stan <dimitrov.s.dev@gmail.com>
2023-02-21 21:08:22 +0200

    Classes_and_Objects_Lab

--- Objects_and_Classes/Exercise/Exercise.py
+++ Objects_and_Classes/Exercise/Exercise.py
@@ -1,23 +1,50 @@
-# Task 1 Storage
+# # Task 1 Storage
+#
+# class Storage:
+#
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.storage = []
+#
+#     def add_product(self, product: str):
+#         if len(self.storage) < self.capacity:
+#             self.storage.append(product)
+#
+#     def get_products(self):
+#         return self.storage
+#
+# storage = Storage(4)
+# storage.add_product("apple")
+# storage.add_product("banana")
+# storage.add_product("potato")
+# storage.add_product("tomato")
+# storage.add_product("bread")
+#
+# print(storage.get_products())
+#
+#
+# # Task 2 Weapon
 
-class Storage:
+class Weapon:
 
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.storage = []
+    def __init__(self, bullets: int):
+        self.bullets = bullets
 
-    def add_product(self, product: str):
-        if len(self.storage) < self.capacity:
-            self.storage.append(product)
+    def shoot(self):
+        if self.bullets:
+            self.bullets -= 1
+            return f"Shooting"
+        return f"no bullets left"
 
-    def get_products(self):
-        return self.storage
+    def __repr__(self):
+        return f"Remaining bullets: {self.bullets}"
 
-storage = Storage(4)
-storage.add_product("apple")
-storage.add_product("banana")
-storage.add_product("potato")
-storage.add_product("tomato")
-storage.add_product("bread")
-
-print(storage.get_products())
+weapon = Weapon(5)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
