8af56b5c3739a64542619483c98ebed41937f340
Stan <dimitrov.s.dev@gmail.com>
2023-02-22 21:43:58 +0200

    Dictionaries_Lab

--- Dictionaries/Lab/Lab.py
+++ Dictionaries/Lab/Lab.py
@@ -13,3 +13,21 @@ print(bakery)
 
 # Task 2 Stock
 
+items = input().split()
+search_products = input().split()
+bakery = {}
+
+for index in range(0, len(items), 2):
+    key = items[index]
+    value = int(items[index + 1])
+    bakery[key] = value
+
+for product in search_products:
+    if product in bakery.keys():
+        quantity = bakery[product]
+        print(f"We have {quantity} of {product} left")
+    else:
+        print(f"Sorry, we don't have {product}")
+
+
+
