b1684663f267e830c34ffd6d6fe313ba3ca60564
Stan <dimitrov.s.dev@gmail.com>
2023-02-23 07:02:21 +0200

    Python_Fundamentals

---
+++ Dictionaries/Exercise/Exercise.py
@@ -0,0 +1,14 @@
+# Task 1 Count Chars in a String
+
+# word = input()
+# result = {}
+#
+# for char in word:
+#     if char not in result:
+#         result[char] = 0
+#     result[char] += 1
+#
+# for key, value in result.items():
+#     print(f"{key} -> {value}")
+
+# Task 2 A Miner Task
