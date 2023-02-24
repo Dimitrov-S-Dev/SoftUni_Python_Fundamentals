370b8e26fa72bbe0e874698ad0266a1be362c175
Stan <dimitrov.s.dev@gmail.com>
2023-02-24 23:04:08 +0200

    Python_Fundamentals

--- Text_Processing/Lab/Lab.py
+++ Text_Processing/Lab/Lab.py
@@ -1,7 +1,15 @@
 # Task 1 Reverse String
+def get_main(lst):
+    for word in lst:
+        print(f"{word} = {word[::-1]}")
+
+
+words = []
 
 while True:
     command = input()
     if command == "end":
         break
-    print(f"{command} = {command[::-1]}")
+    words.append(command)
+
+get_main(words)
