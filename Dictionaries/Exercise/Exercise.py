d1d1602c00225f0c7a567beff64a0db614c6ce4b
Stan <dimitrov.s.dev@gmail.com>
2023-02-24 02:31:46 +0200

    Python_Fundamentals

--- Dictionaries/Exercise/Exercise.py
+++ Dictionaries/Exercise/Exercise.py
@@ -182,20 +182,97 @@
 
 # Task 10 Company Users
 
-result = {}
+# result = {}
+#
+# while True:
+#     command = input()
+#     if command == "End":
+#         break
+#     company_name, employee = command.split(" -> ")
+#     if company_name not in result.keys():
+#         result[company_name] = []
+#     if employee not in result[company_name]:
+#         result[company_name].append(employee)
+#
+# for key in result.keys():
+#     print(f"{key}")
+#     for el in result[key]:
+#         print(f"-- {el}")
 
-while True:
-    command = input()
-    if command == "End":
-        break
-    company_name, employee = command.split(" -> ")
-    if company_name not in result.keys():
-        result[company_name] = []
-    if employee not in result[company_name]:
-        result[company_name].append(employee)
+# Task 11 Force Book
+#
+# result = {}
+#
+# while True:
+#     command = input()
+#     if command == "exam finished":
+#         break
+#     info = command.split("-")
+#     username = info[0]
+#     language = info[1]
+#     points = int(info[2])
+#     if username not in result:
+#         result[username] = {}
+#         result[username][language] = []
+#     result[username][language].append(points)
+#
+# print(f"Results:")
+#
+# print(f"Submissions:")
+#
 
-for key in result.keys():
-    print(f"{key}")
-    for el in result[key]:
-        print(f"-- {el}")
+# Task 11 Force Book
+def get_pipe(force, user, info_dict):
+    if force not in info_dict.keys() and user not in info_dict.values():
+        info_dict[force] = []
+        info_dict[force].append(user)
+    condition = True
+    for names in info_dict.values():
+        if user in names:
+            condition = False
+    if condition:
+        info_dict[force].append(user)
+    return info_dict
+def get_arrow(force, user, info_dict):
+    condtion = True
+    for key,value in info_dict.items():
+        if user in value:
+            condtion = False
+            info_dict[key].remove(user)
+    if condtion:
+        if force not in info_dict.keys():
+            info_dict[force] = []
+        info_dict[force].append(user)
+        print(f"{user} joins the {force} side!")
+    else:
+        if force == "Lighter":
+            info_dict["Lighter"].append(user)
+        else:
+            info_dict["Darker"].append(user)
+
+    return info_dict
+
+
+def get_main():
+    user_info = {}
+    while True:
+        command = input()
+        if command == "Lumpawaroo":
+            break
+
+        if "|" in command:
+            info = command.split(" | ")
+            force = info[0]
+            user = info[1]
+            user_info = get_pipe(force, user, user_info)
+        elif "->" in command:
+            info = command.split(" -> ")
+            force = info[1]
+            user = info[0]
+            user_info = get_arrow(force, user, user_info)
+    return user_info
+
+for key, value in get_main().items():
+    print(f"Side: {key}, Members: {len(value)} ! {'! '.join(value)}")
+
 
