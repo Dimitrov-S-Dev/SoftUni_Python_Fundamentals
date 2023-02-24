640e86f3ecc30ad7993e038ad83416a139c355d7
Stan <dimitrov.s.dev@gmail.com>
2023-02-24 23:18:58 +0200

    Python_Fundamentals

--- Text_Processing/Lab/Lab.py
+++ Text_Processing/Lab/Lab.py
@@ -1,15 +1,25 @@
 # Task 1 Reverse String
-def get_main(lst):
-    for word in lst:
-        print(f"{word} = {word[::-1]}")
+# def get_main(lst):
+#     for word in lst:
+#         print(f"{word} = {word[::-1]}")
+#
+#
+# words = []
+#
+# while True:
+#     command = input()
+#     if command == "end":
+#         break
+#     words.append(command)
+#
+# get_main(words)
+
+# Task 2 Repeat String
+
+string = input().split()
+
+result = [el * len(el) for el in string]
+print("".join(result))
 
 
-words = []
 
-while True:
-    command = input()
-    if command == "end":
-        break
-    words.append(command)
-
-get_main(words)
