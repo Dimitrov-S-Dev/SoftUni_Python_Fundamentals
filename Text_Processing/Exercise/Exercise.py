efede5b29e266ad1c8ce4132caacc2fcd613ac3b
Stan <dimitrov.s.dev@gmail.com>
2023-02-26 00:33:43 +0200

    Python_Fundamentals

--- Text_Processing/Exercise/Exercise.py
+++ Text_Processing/Exercise/Exercise.py
@@ -2,18 +2,43 @@
 # import string
 #
 # def get_main(lst):
-#     flag = 0
 #     expected_element = string.digits + string.ascii_letters + "-" + "_"
 #     for name in lst:
-#         flag = 0
+#         condition = True
 #         if 3 > len(name) or len(name) > 16:
-#             flag = 1
+#             condition = False
 #         elif len([x for x in name if x in expected_element]) != len(name):
-#             flag = 1
-#         elif flag == 0:
+#             condition = False
+#         elif condition:
 #             print(name)
 #
 # username = input().split(", ")
 # get_main(username)
 
 # Task 2 Character Multiplier
+
+# def get_result(first, second):
+#     total_sum = 0
+#     for i in range(len(first)):
+#         if i < len(second):
+#             total_sum += ord(first[i]) * ord(second[i])
+#         else:
+#             total_sum += ord(first[i])
+#     return total_sum
+#
+# def get_multiply(first_word, second_word):
+#     result = 0
+#     if len(first_word) > len(second_word):
+#         result = get_result(first_word, second_word)
+#     else:
+#         result = get_result(second_word, first_word)
+#     print(result)
+#
+#
+# data = input().split()
+# get_multiply(data[0],data[1])
+
+# Task 3 Extract File
+
+
+
