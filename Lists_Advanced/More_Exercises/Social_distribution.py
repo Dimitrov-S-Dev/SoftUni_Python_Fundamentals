82e757c495345456db2a2cb2781f5f3042b2ffb5
Stan <dimitrov.s.dev@gmail.com>
2023-02-26 20:06:27 +0200

    Python_Fundamentals

--- Lists_Advanced/More_Exercises/Social_distribution.py
+++ Lists_Advanced/More_Exercises/Social_distribution.py
@@ -1,16 +1,3 @@
-"""
-On the first line, you will be given the population
-(numbers separated by comma and space ", ").
-On the second line, you will be given the minimum wealth.
-You should distribute the wealth so that no part
-of the population has less than the minimum wealth.
-To do that, you should always take wealth from
-the wealthiest part of the population.
-There will be cases where the distribution
-will not be possible.
-In that case, print: "No equal distribution possible".
-"""
-
 def get_distribution(lst, num):
     max_num = max(lst)
     max_num_index = lst.current_index(max_num)
@@ -24,6 +11,6 @@ def get_distribution(lst, num):
     return lst
 
 
-numbers = list(map(int, input().split(", ")))
+numbers = [int(x) for x in input().split(", ")]
 min_wealth = int(input())
 print(get_distribution(numbers, min_wealth))
