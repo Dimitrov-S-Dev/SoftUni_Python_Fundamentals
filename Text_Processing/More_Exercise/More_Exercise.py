5e3ab7d4419b8e44fda733aa7224712f81ad7043
Stan <dimitrov.s.dev@gmail.com>
2023-02-26 09:32:47 +0200

    Python_Fundamentals

--- Text_Processing/More_Exercise/More_Exercise.py
+++ Text_Processing/More_Exercise/More_Exercise.py
@@ -1,10 +1,25 @@
 # Task 1 Extract Person Information
 
-def get_name_age(text):
-    pass
+# def get_text(text):
+#     name = ""
+#     age = ""
+#     name_start = text.index("@") + 1
+#     name_end = text.index("|")
+#     name += text[name_start:name_end]
+#     age_start = text.index("#") + 1
+#     age_end = text.index("*")
+#     age += text[age_start:age_end]
+#     print(f"{name} is {age} years old.")
+#
+#
+# def get_name_age(number):
+#     for _ in range(n):
+#         current_text = input()
+#         get_text(current_text)
+#
+#
+# n = int(input())
+# get_name_age(n)
 
+# Task 2 ASCII Sumator
 
-n = int(input())
-
-for _ in range(n):
-    current_text = input()
