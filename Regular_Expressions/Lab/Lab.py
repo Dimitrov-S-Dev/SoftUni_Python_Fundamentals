4747d2436acc39cf65a814659ec6ae58c810dfa5
Stan <dimitrov.s.dev@gmail.com>
2023-02-27 20:37:16 +0200

    Python_Fundamentals

--- Regular_Expressions/Lab/Lab.py
+++ Regular_Expressions/Lab/Lab.py
@@ -1,2 +1,19 @@
 # Task 1 Match Full Name
+# import re
+#
+# pattern = r"\b[A-Z][a-z]+ [A-Z][a-z]+\b"
+# names = input()
+#
+# valid_names = re.findall(pattern, names)
+# print("".join(valid_names))
+
+# Task 2 Match Phone Number
+
+# import re
+#
+# phone_numbers = input()
+# pattern = r"\+359-2-\d{3}-\d{4}\b|\+359 2 \d{3} \d{4}\b"
+# valid_numbers = re.findall(pattern, phone_numbers)
+#
+# print(", ".join(valid_numbers))
 
