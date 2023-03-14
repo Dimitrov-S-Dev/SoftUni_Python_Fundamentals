1d8928fd9070d18544ce8e785307d2c97956e67e
Stan <dimitrov.s.dev@gmail.com>
2023-03-14 03:50:28 +0200

    Python_Fundamentals

--- Regular_Expressions/Exercise/Exercise.py
+++ Regular_Expressions/Exercise/Exercise.py
@@ -1,11 +1,22 @@
 import re
 # Task 1 Capture the Numbers
-pattern = r"\d+"
+# pattern = r"\d+"
+#
+# text = input()
+# while text:
+#     match = re.findall(pattern, text)
+#     if match:
+#         print(" ".join(match),end=" ")
+#     text = input()
 
+# Task 2 Find Variable Names in Sentences
+
+pattern = r"\b\_[A-Za-z0-9]+\b"
 text = input()
-while text:
-    match = re.findall(pattern, text)
-    if match:
-        print(" ".join(match),end=" ")
-    text = input()
+
+matches = re.findall(pattern, text)
+result = []
+for match in matches:
+    result.append(match[1:])
+print(",".join(result),end="")
 
