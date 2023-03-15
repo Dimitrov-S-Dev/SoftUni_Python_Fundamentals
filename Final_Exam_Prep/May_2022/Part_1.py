f674996c39be95b40edaebef54b3e00172d47e17
Stan <dimitrov.s.dev@gmail.com>
2023-03-15 22:22:58 +0200

    Final_Exam_Prep

--- Final_Exam_Prep/May_2022/Part_1.py
+++ Final_Exam_Prep/May_2022/Part_1.py
@@ -50,3 +50,41 @@
 # data = input()
 # get_pass(data)
 #
+
+# Task 2 Emoji Detector
+
+import re
+
+words = {}
+name_pattern = r"\s([:*]{2})([A-Za-z]+)\1"
+number_pattern = r"\d"
+text = input()
+animals = []
+count = 1
+name_result = re.finditer(name_pattern, text)
+for name in name_result:
+    a_name = name.group(0)[1:]
+    print(a_name)
+
+number_result = re.findall(number_pattern, text)
+if number_result:
+   count_int = [int(x) for x in number_result]
+   for num in count_int:
+       count *= num
+print(count)
+# cool_animals = []
+# for animal in animals:
+#     curr_sum = 0
+#     for char in animal:
+#         curr_sum += ord(char)
+#     if curr_sum > count:
+#         cool_animals.append(animal)
+# print(f"Cool threshold: {count}")
+# print(f"{len(animals)} emojis found in the text. The cool ones are:")
+# for anim in cool_animals:
+#     print(f"{anim }")
+
+
+
+
+
