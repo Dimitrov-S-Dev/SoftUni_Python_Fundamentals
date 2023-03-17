848f4a965b69460758d4bd3c5331524ee15b0340
Stan <dimitrov.s.dev@gmail.com>
2023-03-17 23:42:30 +0200

    Final_Exam_Prep

--- Final_Exam_Prep/May_2022/Part_2.py
+++ Final_Exam_Prep/May_2022/Part_2.py
@@ -50,22 +50,85 @@
 
 # Task 2
 
-import re
-n = int(input())
-for _ in range(n):
-    curr_barcode = input()
-    pattern = r"(\@\#+)([A-Z][A-Za-z0-9]{4,}[A-Z])"
-    match = re.search(pattern, curr_barcode)
-    if match:
-       num = r"\d"
-       numbers = re.findall(num, match.group())
-       if numbers:
-           print(f"Product group: {''.join(numbers)}")
-       else:
-           print(f"Product group: 00")
-    else:
-        print(f"Invalid barcode")
+# import re
+# n = int(input())
+# for _ in range(n):
+#     curr_barcode = input()
+#     pattern = r"(\@\#+)([A-Z][A-Za-z0-9]{4,}[A-Z])"
+#     match = re.search(pattern, curr_barcode)
+#     if match:
+#        num = r"\d"
+#        numbers = re.findall(num, match.group())
+#        if numbers:
+#            print(f"Product group: {''.join(numbers)}")
+#        else:
+#            print(f"Product group: 00")
+#     else:
+#         print(f"Invalid barcode")
 
-# Task 3
+# Task 3 Need for Speed III
+
+def get_info(number):
+    car_dict = {}
+    for _ in range(number):
+        car, mileage, fuel = input().split("|")
+        mileage = int(mileage)
+        fuel = int(fuel)
+        car_dict[car] = {"mileage": mileage, "fuel": fuel}
+
+    return car_dict
+
+def get_while(dict):
+    while True:
+        command = input()
+        if command == "Stop":
+            break
+        info = command.split(" : ")
+        action = info[0]
+        car = info[1]
+        if action == "Drive":
+            distance = int(info[2])
+            fuel = int(info[3])
+            if dict[car]["fuel"] >= fuel:
+                dict[car]["mileage"] += distance
+                dict[car]["fuel"] -= fuel
+                print(f"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.")
+                if dict[car]["mileage"] >= 100_000:
+                    print(f"Time to sell the {car}!")
+                    del dict[car]
+            else:
+                print(f"Not enough fuel to make that ride")
+
+        elif action == "Refuel":
+            fuel = int(info[2])
+            curr_fuel = dict[car]["fuel"]
+            if curr_fuel + fuel > 75:
+                dict[car]["fuel"] = 75
+            else:
+                dict[car]["fuel"] += fuel
+            diff = dict[car]["fuel"] - curr_fuel
+            print(f"{car} refueled with {diff} liters")
+
+        elif action == "Revert":
+            kilometers = int(info[2])
+            dict[car]["mileage"] -= kilometers
+            if dict[car]["mileage"] < 10_000:
+                dict[car]["mileage"] = 10_000
+            else:
+                print(f"{car} mileage decreased by {kilometers} kilometers")
+    return dict
+
+def get_print(dict):
+    for car in dict.keys():
+        print(f"{car} -> Mileage: {dict[car]['mileage']} kms, Fuel in the tank: {dict[car]['fuel']} lt.")
+def get_need_for_speed(num):
+    cars_dict = get_info(n)
+    cars_dict = get_while(cars_dict)
+    cars_dict = get_print(cars_dict)
+
+
+n = int(input())
+get_need_for_speed(n)
+
 
 
