9dccc2ac2bc8640f59e3a48cf934c6c103dc3033
Stan <dimitrov.s.dev@gmail.com>
2023-03-19 13:18:40 +0200

    Final_Exam_Prep

--- Regular_Expressions/More_Exercise/Tasks.py
+++ Regular_Expressions/More_Exercise/Tasks.py
@@ -1,6 +1,6 @@
 # Task 1 Race
 
-# import re
+import re
 #
 # names = input().split(", ")
 # names_dict = {}
@@ -38,4 +38,29 @@
 #     if count == 3:
 #         break
 
-# Task 2
+# Task 2 SoftUni Bar Income
+barcode_dict = {}
+
+while True:
+    text = input()
+    if text == "end of shift":
+        break
+    pattern = r"%([A-Z][a-z]+)\%[^\|\$\%\.]*?\<(\w+)\>[^\|\$\%\.]*?\|([0-9]+)\|[\w\-]*?([0-9.]+[0-9](?=\$))"
+    matches = re.finditer(pattern, text)
+    if matches:
+        for match in matches:
+            name = match.group(1)
+            product = match.group(2)
+            qty = int(match.group(3))
+            price = float(match.group(4))
+            if name not in barcode_dict.keys():
+                barcode_dict[name]= {product: 0}
+            total_price = qty * price
+            barcode_dict[name][product] += total_price
+            print(f"{name}: {product} - {total_price:.2f}")
+
+total = 0
+for k,v in barcode_dict.items():
+    for sub_k in v.keys():
+        total += barcode_dict[k][sub_k]
+print(f"Total income: {total:.2f}")
