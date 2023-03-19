4637b99738aa14c4fa91be94951280583bb3b3bd
Stan <dimitrov.s.dev@gmail.com>
2023-03-19 20:23:31 +0200

    Final_Exam_Prep

--- Final_Exam_Prep/January_2022/Part_2.py
+++ Final_Exam_Prep/January_2022/Part_2.py
@@ -43,21 +43,85 @@
 # get_world_tour(text)
 
 # Task 2 Ad Astra
+#
+# import re
+#
+# pattern = r"\#([A-Z a-z]+)\#(\d{2}\/\d{2}\/\d{2})\#(\d+)\#|\|([A-Z a-z]+)\|(\d{2}\/\d{2}\/\d{2})\|(\d+)\|"
+# text = input()
+#
+# calories = 0
+# for_print = []
+# matches = re.findall(pattern, text)
+# for match in matches:
+#     curr_match = [elem for elem in match if elem != ""]
+#     calories += int(curr_match[2])
+#     for_print.append(curr_match)
+#
+# days = calories // 2000
+# print(f"You have food to last you for: {days} days!")
+# for elem in for_print:
+#     print(f"Item: {elem[0]}, Best before: {elem[1]}, Nutrition: {elem[2]}")
 
-import re
+# Task 3 P!rates
 
-pattern = r"\#([A-Z a-z]+)\#(\d{2}\/\d{2}\/\d{2})\#(\d+)\#|\|([A-Z a-z]+)\|(\d{2}\/\d{2}\/\d{2})\|(\d+)\|"
-text = input()
+def get_sail():
+    pirates_dict = {}
+    while True:
+        command = input()
+        if command == "Sail":
+            break
+        town, population, gold = command.split("||")
+        population = int(population)
+        gold = int(gold)
+        if town not in pirates_dict.keys():
+            pirates_dict[town] = {"population": 0, "gold": 0}
+        pirates_dict[town]["population"] += population
+        pirates_dict[town]["gold"] += gold
 
-calories = 0
-for_print = []
-matches = re.findall(pattern, text)
-for match in matches:
-    curr_match = [elem for elem in match if elem != ""]
-    calories += int(curr_match[2])
-    for_print.append(curr_match)
+    return  pirates_dict
 
-days = calories // 2000
-print(f"You have food to last you for: {days} days!")
-for elem in for_print:
-    print(f"Item: {elem[0]}, Best before: {elem[1]}, Nutrition: {elem[2]}")
+
+def get_while(pirates_dict):
+    while True:
+        command = input()
+        if command == "End":
+            break
+
+        info = command.split("=>")
+        action = info[0]
+        town = info[1]
+
+        if action == "Plunder":
+            people = int(info[2])
+            gold = int(info[3])
+            pirates_dict[town]["population"] -= people
+            pirates_dict[town]["gold"] -= gold
+            print(f"{town} plundered! {gold} gold stolen, {people} citizens killed.")
+            if pirates_dict[town]["population"] == 0 or pirates_dict[town]["gold"] == 0:
+                del pirates_dict[town]
+                print(f"{town} has been wiped off the map!")
+        elif action == "Prosper":
+            gold = int(info[2])
+            if gold < 0:
+                print(f"Gold added cannot be a negative number!")
+            else:
+                pirates_dict[town]["gold"] += gold
+                print(f"{gold} gold added to the city treasury. {town} now has {pirates_dict[town]['gold']} gold.")
+
+    return pirates_dict
+
+
+def get_print(pirates_dict):
+    if len(pirates_dict) > 0:
+        print(f"Ahoy, Captain! There are {len(pirates_dict)} wealthy settlements to go to:")
+        for town in pirates_dict.keys():
+            print(f"{town} -> Population: {pirates_dict[town]['population']} citizens, Gold: {pirates_dict[town]['gold']} kg")
+    else:
+        print(f"Ahoy, Captain! All targets have been plundered and destroyed!")
+
+def get_pirates():
+    pirates_dict = get_sail()
+    pirates_dict = get_while(pirates_dict)
+    get_print(pirates_dict)
+
+get_pirates()
