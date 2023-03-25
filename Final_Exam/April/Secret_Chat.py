a1347995ffefd8fd65cc07d6636d4141768d431a
Stan <dimitrov.s.dev@gmail.com>
2023-03-25 18:08:45 +0200

    Final_Exam

---
+++ Final_Exam/April/Secret_Chat.py
@@ -0,0 +1,42 @@
+def get_while(message):
+    while True:
+        command = input()
+        if command == "Reveal":
+            break
+        info = command.split(":|:")
+        action = info[0]
+
+        if action == "InsertSpace":
+            index = int(info[1])
+            message = message[0:index] + " " + message[index:]
+            print(message)
+
+        elif action == "Reverse":
+            substring = info[1]
+            if substring in message:
+                message = message.replace(substring,"", 1)
+                message = message + substring[::-1]
+                print(message)
+            else:
+                print(f"error")
+
+        elif action == "ChangeAll":
+            substring = info[1]
+            replacement = info[2]
+            message = message.replace(substring, replacement)
+            print(message)
+
+    return message
+
+
+def get_print(message):
+    print(f"You have a new text message: {message}")
+
+
+def get_secret_chat(message):
+    message = get_while(message)
+    get_print(message)
+
+
+data = input()
+get_secret_chat(data)
