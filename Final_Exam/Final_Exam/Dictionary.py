2ce54cecaadb74af9e5ea9f2a262cfe4618cda17
Stan <dimitrov.s.dev@gmail.com>
2023-04-02 16:06:00 +0300

    Final_Exam

---
+++ Final_Exam/Final_Exam/Dictionary.py
@@ -0,0 +1,44 @@
+def get_test(words_lst, dic):
+    for word in words_lst:
+        if word in dic.keys():
+            print(f"{word}:")
+            for element in dic[word]:
+                print(f"-{element}")
+
+
+
+def get_hand_over(dic):
+    result = []
+    for word in dic.keys():
+        result.append(word)
+    print(" ".join(result))
+
+
+
+def get_dictionary(number):
+    dictionary_dict = {}
+    test_words = []
+    for num in range(number):
+        if num == 0:
+
+            info = input().split(" | ")
+            for element in info:
+                if ":" in element:
+                    word, definitions = element.split(": ")
+                    if word not in dictionary_dict.keys():
+                        dictionary_dict[word] = []
+                    dictionary_dict[word].append(definitions)
+        elif num == 1:
+            words = input().split(" | ")
+            for w in words:
+                test_words.append(w)
+
+        elif num == 2:
+            command = input()
+            if command == "Test":
+                get_test(test_words, dictionary_dict)
+            else:
+                get_hand_over(dictionary_dict)
+
+
+get_dictionary(3)
