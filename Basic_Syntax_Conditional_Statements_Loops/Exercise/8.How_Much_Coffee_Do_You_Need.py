7b8061778d4d6e54aa8a0a37fe5a11f7197ba740
Stan <dimitrov.s.dev@gmail.com>
2023-08-07 17:28:33 +0300

    Python_Fundamentals

---
+++ Basic_Syntax_Conditional_Statements_Loops/Exercise/8.How_Much_Coffee_Do_You_Need.py
@@ -0,0 +1,17 @@
+number_of_coffees = 0
+
+while True:
+    command = input()
+    if command == "END":
+        break
+
+    if command.lower() in ["coding", "dog", "cat", "movie"]:
+        if command.islower():
+            number_of_coffees += 1
+        else:
+            number_of_coffees += 2
+
+if number_of_coffees > 5:
+    print("You need extra sleep")
+else:
+    print(number_of_coffees)
