ffafc5f50c74c965cdec8e70da506535e160dee1
Stan <dimitrov.s.dev@gmail.com>
2023-08-07 13:00:28 +0300

    Python_Fundamentals

--- Basic_Syntax_Conditional_Statements_Loops/Lab/Lab.py
+++ Basic_Syntax_Conditional_Statements_Loops/Lab/Lab.py
@@ -1,18 +1,3 @@
-# Task 4 Even Numbers
-
-count_iter = int(input())
-is_even = True
-
-for _ in range(count_iter):
-    number = int(input())
-    if number % 2 != 0:
-        is_even = False
-        print(f"{number} is odd!")
-        break
-
-if is_even:
-    print("All numbers are even.")
-
 # Task 5.Mid-Exam-Preparation Number Between 1 and 100
 
 while True:
