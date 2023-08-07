6ff57f9f7202fa60c01f84fe114d370737362817
Stan <dimitrov.s.dev@gmail.com>
2023-08-07 15:17:06 +0300

    Python_Fundamentals

---
+++ Basic_Syntax_Conditional_Statements_Loops/Exercise/3.Chat_Codes.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    curr_number = int(input())
+    if curr_number == 88:
+        print("Hello")
+    elif curr_number == 86:
+        print("How are you?")
+    elif curr_number < 88:
+        print("GREAT!")
+    else:
+        print("Bye.")
