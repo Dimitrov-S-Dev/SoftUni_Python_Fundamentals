60affc14dd47b7d5bf3b70606e6b8d4eb428715c
Stan <dimitrov.s.dev@gmail.com>
2023-08-07 12:34:18 +0300

    Python_Fundamentals

--- Basic_Syntax_Conditional_Statements_Loops/Lab/Lab.py
+++ Basic_Syntax_Conditional_Statements_Loops/Lab/Lab.py
@@ -1,13 +1,3 @@
-# Task 3 Word Reverse
-
-word = input()
-# reversed_word = ""
-# for index in range(len(word) - 1, -1, -1):
-# 	reversed_word += word[index]
-# print(reversed_word)
-reversed_word = word[::-1]
-print(reversed_word)
-
 # Task 4 Even Numbers
 
 count_iter = int(input())
