97a78f9e8ef6796a5a6366807094cb0d6683810e
Stan <dimitrov.s.dev@gmail.com>
2023-08-10 14:48:06 +0300

    SoftUni_Python_Fundamentals

--- Data_Types_and_Variables/Lab/5.Special_Numbers.py
+++ Data_Types_and_Variables/Lab/5.Special_Numbers.py
@@ -1,7 +1,10 @@
 for number in range(1, int(input()) + 1):
     sum_of_digit = 0
-    sum_of_digit += number % 10
-    sum_of_digit += number // 10
+    digit = number
+    while digit:
+        last_num = digit % 10
+        sum_of_digit += last_num
+        digit = number // 10
     if sum_of_digit == 5 or sum_of_digit == 7 or sum_of_digit == 11:
         print(f"{number} -> True")
     else:
