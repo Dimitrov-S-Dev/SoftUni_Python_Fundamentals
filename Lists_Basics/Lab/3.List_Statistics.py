16eed3e6f02f6670e62bc1960ddc223a0321c4df
Stan <dimitrov.s.dev@gmail.com>
2023-08-13 05:49:09 +0300

    SoftUni_Python_Fundamentals

---
+++ Lists_Basics/Lab/3.List_Statistics.py
@@ -0,0 +1,16 @@
+n = int(input())
+positive = []
+negative = []
+
+for _ in range(n):
+    curr_num = int(input())
+    if curr_num > 0:
+        positive.append(curr_num)
+    else:
+        negative.append(curr_num)
+
+print(positive)
+print(negative)
+
+print(f'Count of positives: {len(positive)}')
+print(f'Sum of negatives: {sum(negative)}')
