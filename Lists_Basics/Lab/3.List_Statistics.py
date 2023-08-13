bf1a0d9ff90c0c51146e44a126920645fe726a5d
Stan <dimitrov.s.dev@gmail.com>
2023-08-13 05:54:53 +0300

    SoftUni_Python_Fundamentals

--- Lists_Basics/Lab/3.List_Statistics.py
+++ Lists_Basics/Lab/3.List_Statistics.py
@@ -4,7 +4,7 @@ negative = []
 
 for _ in range(n):
     curr_num = int(input())
-    if curr_num > 0:
+    if curr_num >= 0:
         positive.append(curr_num)
     else:
         negative.append(curr_num)
