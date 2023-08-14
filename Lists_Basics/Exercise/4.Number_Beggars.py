1902555dc2a6d687f09c25f249794e3144f74c5b
Stan <dimitrov.s.dev@gmail.com>
2023-08-14 15:51:23 +0300

    SoftUni_Python_Fundamentals

--- Lists_Basics/Exercise/4.Number_Beggars.py
+++ Lists_Basics/Exercise/4.Number_Beggars.py
@@ -9,3 +9,17 @@ for beggar in range(beggars_count):
     result.append(beggar_amount)
 
 print(result)
+
+'''
+numbers = input().split(', ')
+beggars_count = int(input())
+
+beggars = [0] * beggars_count
+
+for idx in range(len(numbers)):
+    beggar_idx = idx % beggars_count
+    num = numbers[idx]
+    beggars[beggar_idx] += num
+    
+print(beggars)
+'''
