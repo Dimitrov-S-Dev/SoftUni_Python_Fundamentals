66edee3e9083147900bc446877620ed9f1958ef9
Stan <dimitrov.s.dev@gmail.com>
2023-08-14 18:15:22 +0300

    SoftUni_Python_Fundamentals

---
+++ Lists_Basics/Exercise/10.Bread_Factory.py
@@ -0,0 +1,39 @@
+
+events = input().split('|')
+energy = 100
+coins = 100
+is_bakery_closed = False
+
+for event in events:
+    info = event.split('-')
+    order = info[0]
+    number = int(info[1])
+    if order == 'rest':
+        current_energy = energy
+        if current_energy + number > 100:
+            gained_energy = 100 - energy
+        else:
+            gained_energy = number
+        energy += gained_energy
+        print(f"You gained {gained_energy} energy.")
+        print(f"Current energy: {energy}.")
+    elif order == 'order':
+        if energy >= 30:
+            energy -= 30
+            coins += number
+            print(f"You earned {number} coins.")
+        else:
+            energy += 50
+            print('You had to rest!')
+    else:
+        if coins >= number:
+            coins -= number
+            print(f"You bought {order}.")
+        else:
+            print(f"Closed! Cannot afford {order}.")
+            is_bakery_closed = True
+            break
+if not is_bakery_closed:
+    print('Day completed!')
+    print(f"Coins: {coins}")
+    print(f"Energy: {energy}")
